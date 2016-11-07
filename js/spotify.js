b9ec17f0304aa2936c7e61d09177f9e9cb65394b
Michael Lai <mike24567@gmail.com>
2016-11-07 09:53:47 -0500

    finished setting up apiUrl

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,37 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
-
-
 function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+  return apiUrl = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
 }
-
 
 function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+  return apiUrl = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
 }
+
+//Meat of the Functionality
+$(".submit").on("click", () => {
+  event.preventDefault()
+  let keyword = $("#search-keyword").val()
+  var searchTerm = $("select option:selected").val()
+
+  //Determines Artist or Track search
+  if (searchTerm  === "artist") {
+    searchByArtist(keyword)
+  } else if (searchTerm === "track"){
+    searchByTrack(keyword)
+    }
+
+  //Makes Ajax calls
+  $.ajax({
+    type: "GET",
+    url: apiUrl,
+    dataType: "json"
+  }).done((response) => {
+    console.log("success")
+    
+
+
+  }).fail((response) => {
+    console.log("ajax failed")
+  })
+})
